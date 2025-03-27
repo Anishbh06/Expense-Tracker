2e66c0f6e6b8c84b992ed5dc91931b196c9cf608
Anishbh06 <anishbharish@gmail.com>
2025-03-27 12:11:53 +0530

    Update settings.py

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1r&%w_+97gl+axz7=8i$$!z_0u$jviq)stt#)tmipr_==$ochp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.10.14', 'localhost', '127.0.0.1']
 
